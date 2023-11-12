aa9ae94001cbc46697cf8524764438dc978f501e
Giuseppe Abrami <abrami@em.uni-frankfurt.de>
2023-11-12 19:36:03 +0100

    update

--- src/main/java/org/texttechnologylab/project/data/Gruppe.java
+++ src/main/java/org/texttechnologylab/project/data/Gruppe.java
@@ -4,12 +4,24 @@ import java.util.Set;
 
 /**
  * Interface einer Gruppe
- *
  * @author Giuseppe Abrami
  */
 public interface Gruppe extends BundestagObject{
 
+    /**
+     * Liste aller Mitglieder
+     *
+     * @return
+     */
     Set<Abgeordneter> getMembers();
+
+    /**
+     * Liste aller Mitglieder gemäß Wahlperiode
+     *
+     * @param pPeriode
+     * @return
+     */
+    Set<Abgeordneter> getMembers(Wahlperiode pPeriode);
 
 
 }
